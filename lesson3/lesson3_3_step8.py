8f30ca8817ff1229f725740d5cf6b91cb6240c0d
ja <vpaw6@ya.ru>
2019-07-25 16:29:20 +0300

    add task for lesson3.3_step8

---
+++ lesson3/lesson3_3_step8.py
@@ -0,0 +1,12 @@
+import re
+import sys
+
+#pattern = "(\W+|\s+|^)cat(\W+|\s+|$)"
+#pattern = r"(\b|\W+)cat(\b|\W+)"
+pattern = r"(\bcat\b)"
+
+for line in sys.stdin:
+    line = line.rstrip()
+    #print(re.findall(pattern, line))
+    if re.findall(pattern, line):
+        print(line)
