9a9a600770981a49e33fa2ac4e1b4b11c0f8086e
ja <vpaw6@ya.ru>
2019-07-29 09:56:06 +0300

    add task for lesson3.3_step11

--- lesson3/lesson3_3_step11.py
+++ lesson3/lesson3_3_step11.py
@@ -2,10 +2,10 @@ import re
 import sys
 
 #pattern = "(\W+|\s+|^)cat(\W+|\s+|$)"
-pattern = r"(\b|\W+)(\w+)\2(\b|\W+)"
+#pattern = r"(\b|\W+)(\w+)\2(\b|\W+)"
 
 for line in sys.stdin:
     line = line.rstrip()
     #print(re.findall(pattern, line))
-    if re.findall(pattern, line):
-        print(line)
+    if re.findall('human', line, re.IGNORECASE):
+        print(re.sub('human', 'computer', line))
