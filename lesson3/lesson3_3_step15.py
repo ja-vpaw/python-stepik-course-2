1bc6d904c2011c5c0a84ef4c132eb189459ae97c
ja <vpaw6@ya.ru>
2019-07-29 15:37:20 +0300

    add task for lesson3.3_step14

---
+++ lesson3/lesson3_3_step15.py
@@ -0,0 +1,11 @@
+import re
+import sys
+
+
+pattern = r"(1(01*0)*1|0)*" # from stackoverflow
+#pattern = r"(0|1)+"
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.fullmatch(pattern, line):
+        print(line)
